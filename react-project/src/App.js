e7b96e8cfb5eef6d30c5bf093a31ffc384b1a316
Semion <semionkrutsikau@gmail.com>
2022-09-06 15:32:46 +0300

    Created 2 custom hooks. Install axios for requests.

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -1,45 +1,27 @@
-import React, {useMemo, useState} from "react"
+import React, { useState} from "react"
 import PostForm from "./components/PostForm";
 import "./styles/App.css"
 import PostList from "./components/PostList";
 import PostFilter from "./components/PostFilter";
 import MyModal from "./UI/Modal/MyModal";
 import MyButton from "./UI/button/MyButton";
+import {usePosts} from "./hooks/usePosts";
+
 
 
 function App() {
-    const [posts, setPosts] = useState([
-        {id: 1, title: "Js", body: "ABout JS"},
-        {id: 2, title: "Js", body: "ABout JS"},
-        {id: 3, title: "Js", body: "ABout JS"},
-        {id: 4, title: "Js", body: "ABout JS"},
-
-
-    ]);
-
+    const [posts, setPosts] = useState([]);
     const [filter, setFilter] = useState({sort: "", query: ""})
-
     const [modal, setModal] = useState(false)
-
-
-    const sortedPosts = useMemo(() => {
-        if (filter.sort) {
-            return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]));
-        } else
-            return posts;
-    }, [filter.sort, posts]);
-
-
-    const sortedAndSearchedPosts = useMemo(() => {
-        return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query.toLowerCase()))
-
-    }, [filter.query, sortedPosts]);
+    const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
 
     const newPost = (newPostData) => {
         setPosts([...posts, newPostData])
         setModal(false)
 
     };
+
+
     const removePost = (post) => {
         setPosts(posts.filter(p => p.id !== post.id))
 
